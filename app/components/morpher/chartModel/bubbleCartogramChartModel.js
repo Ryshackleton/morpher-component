f996b8b5ea8dcf36917f67b9e2908446a90e9852
Ryan Shackleton <ryshack@uw.edu>
2019-02-05 02:03:04 -0800

    For bubble cartograms, filter out invalid nodes: those for which valid x, y, and radius properties can be computed.

--- app/components/morpher/chartModel/bubbleCartogramChartModel.js
+++ app/components/morpher/chartModel/bubbleCartogramChartModel.js
@@ -1,9 +1,26 @@
+import { keyBy } from 'lodash';
 import { chartShape } from '../constants';
 import mapChartModel from './mapChartModel';
+import { dataWithValidGetterFunctions } from '../utils';
 
 export default function bubbleCartogramChartModel(chartState) {
+  const {
+    xFromId,
+    yFromId,
+    radiusFromId,
+    dataFiltered,
+    ...rest
+  } = mapChartModel(chartState);
+
+  const validData = dataWithValidGetterFunctions(dataFiltered, [xFromId, yFromId, radiusFromId]);
+
   return {
-    ...mapChartModel(chartState),
+    ...rest,
     shape: chartShape.BUBBLE_CARTOGRAM,
+    xFromId,
+    yFromId,
+    radiusFromId,
+    dataFiltered: validData,
+    dataFilteredById: keyBy(validData, 'morphableId'),
   };
 }
