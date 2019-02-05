02feafb7deeb20892ffd09762406370a258881ad
Ryan Shackleton <ryshack@uw.edu>
2019-02-05 02:38:00 -0800

    Filter out everything except admin0 level countries upon data fetch.

--- app/data/processTopojson.js
+++ app/data/processTopojson.js
@@ -1,46 +1,47 @@
 import { reduce } from 'lodash';
 
-export default function processTopojson(topology) {
+export default function processTopojson(topology, includeLayers) {
   const previousDisputedLayer = {};
 
   topology.objects = reduce(topology.objects, (results, value, key) => {
-    const isDisputedLayer = key.includes('disputes');
+    if (includeLayers && includeLayers.includes(key)) {
+      const isDisputedLayer = key.includes('disputes');
 
-    value.geometries = value.geometries.reduce((geometries, feature, index) => {
-      const { disputes = [], loc_id, admin_id } = feature.properties;
-      const location_id = (admin_id || loc_id) || {};
-      const geomKey = `key-${index + 1}`;
-      let properties;
+      value.geometries = value.geometries.reduce((geometries, feature, index) => {
+        const { disputes = [], loc_id, admin_id } = feature.properties;
+        const location_id = (admin_id || loc_id) || {};
+        const geomKey = `key-${index + 1}`;
+        let properties;
 
-      if (isDisputedLayer) {
-        properties = {
-          key: geomKey,
-          loc_id: (location_id) || previousDisputedLayer[loc_id].loc_id,
-          disputes: disputes.map(Number),
-        };
+        if (isDisputedLayer) {
+          properties = {
+            key: geomKey,
+            loc_id: (location_id) || previousDisputedLayer[loc_id].loc_id,
+            disputes: disputes.map(Number),
+          };
 
-        previousDisputedLayer[loc_id] = properties;
-      } else if (key === 'admin0' || location_id) {
-        properties = {
-          key: geomKey,
-          loc_id,
-        };
+          previousDisputedLayer[loc_id] = properties;
+        } else if (key === 'admin0' || location_id) {
+          properties = {
+            key: geomKey,
+            loc_id,
+          };
+        }
+
+        if (properties) {
+          geometries.push({
+            ...feature,
+            properties,
+          });
+        }
+
+        return geometries;
+      }, []);
+
+      if (value.geometries.length) {
+        results[key] = value;
       }
-
-      if (properties) {
-        geometries.push({
-          ...feature,
-          properties,
-        });
-      }
-
-      return geometries;
-    }, []);
-
-    if (value.geometries.length) {
-      results[key] = value;
     }
-
     return results;
   }, {});
 }
--- app/data/promises/worldTopology.js
+++ app/data/promises/worldTopology.js
@@ -9,7 +9,8 @@ const { PATH_TO_DIST_DATA } = config;
 export default async function () {
   const topojson = await json(`${PATH_TO_DIST_DATA}/world-topo-no-disputes.json`);
 
+  const includeLayers = ['admin0'];
   const topology = presimplify(topojson);
-  processTopojson(topology); // mutates topology
+  processTopojson(topology, includeLayers); // mutates topology
   return topology;
 }
