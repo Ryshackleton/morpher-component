ec93bc0cb8a33b51feee24e8f0b6811d580a2677
Ryan Shackleton <ryshack@uw.edu>
2019-02-05 23:13:04 -0800

    Move the functionality for computing centroids from geographic coordinates to the bubble cartogram so centroids don't get computed unnecessarily.

--- app/components/morpher/chartModel/bubbleCartogramChartModel.js
+++ app/components/morpher/chartModel/bubbleCartogramChartModel.js
@@ -1,18 +1,60 @@
-import { keyBy } from 'lodash';
+import {
+  get, isEmpty, isNil, keyBy, reduce,
+} from 'lodash';
+import { extent, scaleLinear } from 'd3';
 import { chartShape } from '../constants';
 import mapChartModel from './mapChartModel';
-import { dataWithValidGetterFunctions } from '../utils';
+import { dataWithValidGetterFunctions, values } from '../utils';
 
-export default function bubbleCartogramChartModel(chartState) {
+export default function bubbleCartogramChartModel(chartState, computeRadii = true) {
   const {
-    xFromId,
-    yFromId,
-    radiusFromId,
+    chartRequest: {
+      radiusRange = [2, 20],
+      radiusField,
+    },
     dataFiltered,
+    dataFilteredById,
+    projection,
+    idFeatureMap,
     ...rest
   } = mapChartModel(chartState);
 
-  const validData = dataWithValidGetterFunctions(dataFiltered, [xFromId, yFromId, radiusFromId]);
+  /** pre compute id -> centroids for tweening efficiency */
+  const idCentroidMap = reduce(dataFiltered, (acc, datum) => {
+    acc[datum.morphableId] = projection.path.centroid(idFeatureMap[datum.morphableId]);
+    return acc;
+  }, {});
+  const xFromId = (id) => {
+    return idCentroidMap[id] ? idCentroidMap[id][0] : 0;
+  };
+  const yFromId = (id) => {
+    return idCentroidMap[id] ? idCentroidMap[id][1] : 0;
+  };
+
+  let radiusFromId;
+  const functionsThatShouldReturnNumbers = [xFromId, yFromId];
+  if (computeRadii) {
+    /** pre-compute morphableId -> radius, radius value */
+    const radiusUndefined = isEmpty(radiusField) || isNil(radiusField);
+    const radiusScale = scaleLinear()
+      .domain(extent(values(dataFiltered, radiusField)))
+      .range(radiusRange);
+    const radiusValueFromId = (id) => {
+      return get(dataFilteredById, [id, radiusField], 0);
+    };
+    const idRadiusMap = reduce(dataFiltered, (acc, datum) => {
+      if (!radiusUndefined) {
+        acc[datum.morphableId] = radiusScale(radiusValueFromId(datum.morphableId));
+      }
+      return acc;
+    }, {});
+
+    radiusFromId = (id) => { return idRadiusMap[id]; };
+
+    functionsThatShouldReturnNumbers.push(radiusFromId);
+  }
+
+  const validData = dataWithValidGetterFunctions(dataFiltered, functionsThatShouldReturnNumbers);
 
   return {
     ...rest,
--- app/components/morpher/chartModel/mapChartModel.js
+++ app/components/morpher/chartModel/mapChartModel.js
@@ -1,73 +1,25 @@
-import {
-  get, isEmpty, isNil, keyBy, reduce,
-} from 'lodash';
-import {
-  extent, scaleLinear,
-} from 'd3';
-import { values } from '../utils';
+import { keyBy } from 'lodash';
 import { chartShape } from '../constants';
 
 export default function mapChartModel(chartState) {
   const {
-    chartRequest: {
-      radiusRange = [2, 20],
-      radiusField,
-    },
-    colorScale,
-    colorValueFromId,
-    colorFromId,
     dataFiltered,
-    dataFilteredById,
     idFeatureMap,
     projection,
-    seriesKeys,
+    ...rest
   } = chartState;
 
   /** main function to get geo path from a given morphable id in the projection */
   const pathFromId = (id) => { return projection.path(idFeatureMap[id]); };
 
-  /** pre compute id -> centroids for tweening efficiency */
-  const idCentroidMap = reduce(dataFiltered, (acc, datum) => {
-    acc[datum.morphableId] = projection.path.centroid(idFeatureMap[datum.morphableId]);
-    return acc;
-  }, {});
-  const xFromId = (id) => {
-    return idCentroidMap[id] ? idCentroidMap[id][0] : 0;
-  };
-  const yFromId = (id) => {
-    return idCentroidMap[id] ? idCentroidMap[id][1] : 0;
-  };
-
-  /** pre-compute morphableId -> radius, radius value */
-  const radiusUndefined = isEmpty(radiusField) || isNil(radiusField);
-  const radiusScale = scaleLinear()
-    .domain(extent(values(dataFiltered, radiusField)))
-    .range(radiusRange);
-  const radiusValueFromId = (id) => {
-    return get(dataFilteredById, [id, radiusField], 0);
-  };
-  const idRadiusMap = reduce(dataFiltered, (acc, datum) => {
-    if (!radiusUndefined) {
-      acc[datum.morphableId] = radiusScale(radiusValueFromId(datum.morphableId));
-    }
-    return acc;
-  }, {});
-
-  const radiusFromId = (id) => { return idRadiusMap[id]; };
-
-  /** return the maps that get paths, colors from morphable id's */
+  /** return the maps that get paths morphable id's */
   return {
+    ...rest,
     shape: chartShape.MAP,
-    pathFromId,
-    xFromId,
-    yFromId,
-    radiusFromId,
-    radiusValueFromId,
-    colorScale,
-    colorValueFromId,
-    colorFromId,
-    seriesKeys,
     dataFiltered,
     dataFilteredById: keyBy(dataFiltered, 'morphableId'),
+    pathFromId,
+    idFeatureMap,
+    projection,
   };
 }
